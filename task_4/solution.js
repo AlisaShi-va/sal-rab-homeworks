6b19aee1a6246c6ff647b3e24d34233f7e9832e2
Alisa Shiriaeva <alisa.shiryaewa@yandex.ru>
2023-04-18 22:34:48 +0300

    task №4

--- task_4/solution.js
+++ task_4/solution.js
@@ -4,12 +4,20 @@ function parseProducts(json) {
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
-    // в объект с помощью функции JSON.parse(json)
-    // и запишите в переменную data
-    // Верните как результат функции свойство products объекта data
+     // в объект с помощью функции JSON.parse(json)
+    let data = JSON.parse(json); 
+    let products = data.products; // и запишите в переменную data
+    return products; // Верните как результат функции свойство products объекта data
 }
 
-// Напишите функцию renderProductsCards(json)
+function renderProductsCards(json) {
+    clearProducts();
+    let products = parseProducts(json);
+    length = products.length;
+    for (let i = 0; i < length; i += 1){
+        addProduct(products[i]);
+    }
+} // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
 // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
