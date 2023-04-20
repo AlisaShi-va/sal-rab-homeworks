541d54ac91f959a2bad8bed5c0d2b21d61e10890
Alisa Shiriaeva <alisa.shiryaewa@yandex.ru>
2023-04-20 10:32:13 +0300

    final task

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,3 +1,23 @@
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {client: {name, phone}, order: {address, sum}, goods: []};
+    
+    let countOfGoods = goods.length;
+    
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
+    }
+
+   
+    data.order.address = "ул. " + address.street, "дом" + address.house, "подъезд" + address.entrance, "этаж" + address.floor, "квартира" + address.flat;
+    data.order.sum = sum;
+    data.client = `${name}` + `${phone}`;
+
+
+
+    let jsonData = JSON.stringify({data});
+
+    return jsonData;
+}
 // Исправьте функцию sendRequest
 // Аргументы функции:
 // - имя клиента
